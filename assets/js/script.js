efd360a8a58cb9dc2df6f7b23cc19062ea8d5c30
Davide2410 <92105511+Davide2410@users.noreply.github.com>
2022-10-19 19:32:34 +0200

    pallino first  di default orange

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,6 @@ document.getElementById('myButton').onclick = function () {
     mylogo.classList.toggle('fade');
 }
 
-// funzione per cambio colore radio input
-function changeColor() {
-    document.getElementById('label_gray').style.backgroundColor = 'orangered';
-}
-window.onload = changeColor();
-document.getElementById('click').addEventListener('click', () => {
-    document.getElementById('label_gray').style.backgroundColor = 'lightgray';
-})
-document.getElementById('label_gray').addEventListener('click', () => {
-    document.getElementById('label_gray').style.backgroundColor = 'orangered';
-})
+
+
+
