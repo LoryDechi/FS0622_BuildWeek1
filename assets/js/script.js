65cf34aa1ffc34ba9d434586ce4098e504769368
Lorenzo De Carolis <lorenzodc@hotmail.itm>
2022-10-19 19:10:57 +0200

    modifiche menu tendina e menu cibi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
-window.onload = function changeColor() {
-    var img_1 = document.getElementById('img_1')
-    img_1.style.backgroundColor = '#e94c3d';
+let mylogo = document.getElementById('myLogo');
+document.getElementById('myButton').onclick = function () {
+    mylogo.classList.toggle('fade');
 }
+
+// funzione per cambio colore radio input
+function changeColor() {
+    document.getElementById('label_gray').style.backgroundColor = 'orangered';
+}
+window.onload = changeColor();
+document.getElementById('click').addEventListener('click', () => {
+    document.getElementById('label_gray').style.backgroundColor = 'lightgray';
+})
+document.getElementById('label_gray').addEventListener('click', () => {
+    document.getElementById('label_gray').style.backgroundColor = 'orangered';
+})
