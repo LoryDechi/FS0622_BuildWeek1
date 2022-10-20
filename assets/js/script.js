2c4dedbb7470b7ae70b429fe862f495e74fbdd0e
Lorenzo De Carolis <lorenzodc@hotmail.itm>
2022-10-20 12:29:21 +0200

    pulizia CSS, inserito JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,46 @@
-let mylogo = document.getElementById('myLogo');
-document.getElementById('myButton').onclick = function () {
-    mylogo.classList.toggle('fade');
+//FUNZIONE PER L'APERTURA DEL MENU A TENDINA
+var menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+function togglemenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "400px";
+    }
+    else {
+        menuList.style.maxHeight = "0px";
+    }
 }
+
+// FUNZIONE CHE REGOLA IL CAMBIO COLORE DEI DOTS NELLO SLIDER
+function changeColor() {
+    document.getElementById('label_gray').style.backgroundColor = 'orangered';
+}
+window.onload = changeColor();
+document.getElementById('click').addEventListener('click', () => {
+    document.getElementById('label_gray').style.backgroundColor = 'lightgray';
+})
+document.getElementById('label_gray').addEventListener('click', () => {
+    document.getElementById('label_gray').style.backgroundColor = 'orangered';
+})
+
+
+// var slideImg = 1;
+// showDivs(slideImg);
+
+// function slideShow(n) {
+//     showDivs(slideImg += n);
+// }
+
+// function showDivs(n) {
+//     var i;
+//     var x = document.getElementsByClassName("radio");
+//     if (n > x.length) { slideImg = 1 }
+//     for (i = 0; i < x.length; i++) {
+//         x[i].ariaChecked = mixed;
+//     }
+
+// }
+
+
 
 
 
